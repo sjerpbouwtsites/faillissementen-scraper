259c244d106003d06a335d14396eb5b04b4c997e
Sjerp van Wouden <dev@sjerpbouwtsites.nl>
2020-10-18 03:42:38 +0200

    node versie conform plesk versie

--- index.js
+++ index.js
@@ -10,22 +10,22 @@
 /**
  * Als lager dan versie 14, niet draaien.
  */
-// try {
-//   const nodeversie = Number(process.versions.node.split('.')[0]);
-// console.log('node versie: ', nodeversie)
-//   if (typeof nodeversie !== 'number') {
-//     throw new Error({
-//       message: 'process object anders dan verwacht',
-//       process: process,
-//     })
-//   }
-//   if (nodeversie < 14) {
-//     throw new Error('node versie te laag. is22: ', nodeversie);
-//   }
-// } catch (error) {
-//   console.error(error);
-//   process.exit();
-// }
+try {
+  const nodeversie = Number(process.versions.node.split(".")[0]);
+  console.log("node versie: ", nodeversie);
+  if (typeof nodeversie !== "number") {
+    throw new Error({
+      message: "process object anders dan verwacht",
+      process: process,
+    });
+  }
+  if (nodeversie !== 12) {
+    throw new Error("de server draait op Node 12 aangezien plesk stom is. Graag ook lokaal gebruiken");
+  }
+} catch (error) {
+  console.error(error);
+  process.exit();
+}
 
 // function pakDagenVerzameling(start, end) {
 //   for (var arr = [], dt = new Date(start); dt <= end; dt.setDate(dt.getDate() + 1)) {
